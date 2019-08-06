cf1f74176ee79b996c4cd4b62850161d9a07ec4d
nnijat <nn.aca2019@gmail.com>
2019-08-05 21:16:10 -0500

    Created component files, cut the proper HTML, replayed with correct component tag

---
+++ public/js/src/components/TasksPanel.js
@@ -0,0 +1,20 @@
+function TasksPanel(props) {
+    return (
+        <div className="panel panel-default">
+            <div className="panel-heading">
+                <h3 className="panel-title"><i className="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
+            </div>
+            <div className="panel-body">
+                <div className="list-group">
+                    {/*   <TaskItem>    */}
+                    <TaskItem></TaskItem>
+
+                </div>
+                <div className="text-right">
+                    <a href="#">View All Activity <i className="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- public/js/src/index.js
+++ public/js/src/index.js
@@ -1,27 +1,27 @@
 
 const {
-dateTime,
-newComments,
-newTasks,
-newOrders,
-tickets,
-orders,
-taskItem,
-tasks,
-messages,
+  dateTime,
+  newComments,
+  newTasks,
+  newOrders,
+  tickets,
+  orders,
+  taskItem,
+  tasks,
+  messages,
 } = state;
 
 ReactDOM.render(
-  <App 
+  <App
     taskItem={taskItem}
     dateTime={dateTime}
     newComments={newComments}
     newTasks={newTasks}
     newOrders={newOrders}
     tickets={tickets}
-    orders={orders} 
+    orders={orders}
     tasks={tasks}
     messages={messages}
-    />,
+  />,
   document.getElementById("root")
 );
